4d07f8426809f0564d00ca999360ee550aa46660
fu-kunn <sky53908@gmail.com>
2022-12-28 17:09:06 +0900

    [Add]ルートの追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, Flaskbook!"
+
+
+# @app.route("/hello",
+#   methods=["GET"],
+#   endopoint="hello-endopoint")
+# def hello():
+#     return "Hello, World!"
+
+@app.route("/hello")
+def hello():
+    return "Hello, World!"
