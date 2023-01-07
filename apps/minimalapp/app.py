4fb9025997a3004cc1759d2d8e85f9b362539e0c
fu-kunn <sky53908@gmail.com>
2023-01-07 15:22:13 +0900

    [Add]URLの作成

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,12 +6,23 @@ app = Flask(__name__)
 def index():
     return "Hello, Flaskbook!"
 
+# エンドポイントの作成
 @app.route("/hello/<name>",
   methods=["GET", "POST"],
   endpoint="hello-endpoint")
 def hello(name):
     return f"Hello, {name}!"
 
-# @app.route("/hello")
-# def hello():
-#     return "Hello, World!"
+# show_nameエンドポイントの作成
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
