30c0f295bad444a4748f68f955f359e3e8ac91a2
fu-kunn <sky53908@gmail.com>
2023-01-07 14:48:30 +0900

    [Add]POSTメソッドの追加

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -7,12 +7,12 @@ def index():
     return "Hello, Flaskbook!"
 
 
-# @app.route("/hello",
-#   methods=["GET"],
-#   endopoint="hello-endopoint")
-# def hello():
-#     return "Hello, World!"
-
-@app.route("/hello")
+@app.route("/hello",
+  methods=["GET", "POST"],
+  endpoint="hello-endpoint")
 def hello():
     return "Hello, World!"
+
+# @app.route("/hello")
+# def hello():
+#     return "Hello, World!"
