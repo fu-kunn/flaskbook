930dd8f51afdad2349bd350d1ad4ee52f24e914e
fu-kunn <sky53908@gmail.com>
2023-01-17 19:50:30 +0900

    [Add]falsk-alchemyとflask=migrateの利用準備

--- apps/app.py
+++ apps/app.py
@@ -1,13 +1,28 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
 
 # create_app関数の作成
 def create_app():
     # Flaskインスタンス生成
     app = Flask(__name__)
+    # アプリのコンフィグ設定
+    app.config.from_mapping(
+        SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
+        SQLAlCHEMY_DATABASE_URI = f"sqlite://{Path(__file__).parent.parent / 'local.sqlite'}", SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    # SQLAlchemyとアプリを連携
+    db.init_app(app)
+    # Migrateとアプリ連携
+    Migrate(app, db)
 
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
-
     # register_blueprintを使いviewsのcrudをアプリへ登録する
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
