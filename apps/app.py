18d41333e1283046fc7fcfc02846a1d0046fb34a
fu-kunn <sky53908@gmail.com>
2023-01-23 17:03:11 +0900

    [Add]CSRF対策をするための設定

--- apps/app.py
+++ apps/app.py
@@ -2,9 +2,13 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
+
+# CSRFProtectをインスタンス化
+csrf = CSRFProtect()
 
 # create_app関数の作成
 def create_app():
@@ -15,13 +19,16 @@ def create_app():
         SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
         SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}", 
         SQLALCHEMY_TRACK_MODIFICATIONS = False,
-        SQLALCHEMY_ECHO=True
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f"
     )
 
     # SQLAlchemyとアプリを連携
     db.init_app(app)
     # Migrateとアプリ連携
     Migrate(app, db)
+    # csrf.init_app関数を使ってアプリ連携
+    csrf.init_app(app)
 
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
