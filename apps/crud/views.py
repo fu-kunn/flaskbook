a361f49d85714a453f3c36fe29f740ca996e2467
fu-kunn <sky53908@gmail.com>
2023-01-24 17:33:54 +0900

    [Add]ユーザ一覧のエンドポイントを作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,4 @@
-from app.crud.forms import UserForm
+from apps.crud.forms import UserForm
 from flask import Blueprint, render_template, redirect, url_for
 from apps.app import db
 from apps.crud.models import User
@@ -18,7 +18,7 @@ def index():
 
 @crud.route("/sql")
 def sql():
-    db.session.query(User).limit(1).all()
+    db.session.query(User).all()
     return "コンソールログを確認してください"
 
 @crud.route("/users/new", methods=["GET", "POST"])
@@ -26,7 +26,7 @@ def create_user():
     # UserFormをインスタンス化する
     form = UserForm()
     # フォームの値をバリデートする
-    if form.vaildate_on_submit():
+    if form.validate_on_submit():
         # ユーザ作成
         user = User(
             username=form.username.data,
@@ -39,3 +39,10 @@ def create_user():
         # ユーザの一覧画面へリダイレクト
         return redirect(url_for("crud.users"))
     return render_template("crud/create.html", form=form)
+
+
+@crud.route("/users")
+def users():
+    """ユーザの一覧を取得する"""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
