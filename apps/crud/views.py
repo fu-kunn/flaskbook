471514e66126fa6315bd75330b789d250f6d6481
fu-kunn <sky53908@gmail.com>
2023-01-26 22:54:00 +0900

    [Add]共通テンプレートのファイル作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -67,7 +67,7 @@ def edit_user(user_id):
     # GETの場合はHTMLを返す
     return render_template("crud/edit.html", user=user, form=form)
 
-@crud.route("/user/<user.id>/delete", methods=["POST"])
+@crud.route("/user/<user_id>/delete", methods=["POST"])
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
