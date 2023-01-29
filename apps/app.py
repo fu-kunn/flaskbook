929118ae8992ccb36f644eb9a1863ab2ff6ce285
fu-kunn <sky53908@gmail.com>
2023-01-29 18:53:13 +0900

    [Add]コンフィグオブジェクトの読み込み

--- apps/app.py
+++ apps/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
@@ -36,3 +37,11 @@ def create_app():
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
     return app
+
+
+# コンフィグのキーを渡す
+def create_app(config_key):
+    app = Flask(__name__)
+
+    # config_keyにマッチする環境のコンフィグクラスを読み込む
+    app.config.from_object(config[config_key])
