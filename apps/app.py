6e4b5959d69da3c8c00b1a654cdf25d99a267ffe
fu-kunn <sky53908@gmail.com>
2023-01-30 09:44:13 +0900

    [Fix]既存のapp.config.from_mappingを削除

--- apps/app.py
+++ apps/app.py
@@ -12,18 +12,25 @@ db = SQLAlchemy()
 csrf = CSRFProtect()
 
 # create_app関数の作成
-def create_app():
-    # Flaskインスタンス生成
-    app = Flask(__name__)
-    # アプリのコンフィグ設定
-    app.config.from_mapping(
-        SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}", 
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f"
-    )
+# def create_app():
+#     # Flaskインスタンス生成
+#     app = Flask(__name__)
+#     # アプリのコンフィグ設定
+#     app.config.from_mapping(
+#         SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
+#         SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}", 
+#         SQLALCHEMY_TRACK_MODIFICATIONS = False,
+#         SQLALCHEMY_ECHO=True,
+#         WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f"
+#     )
 
+# コンフィグのキーを渡す
+def create_app(config_key):
+    app = Flask(__name__)
+
+    # config_keyにマッチする環境のコンフィグクラスを読み込む
+    app.config.from_object(config[config_key])
+    
     # SQLAlchemyとアプリを連携
     db.init_app(app)
     # Migrateとアプリ連携
@@ -37,11 +44,3 @@ def create_app():
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
     return app
-
-
-# コンフィグのキーを渡す
-def create_app(config_key):
-    app = Flask(__name__)
-
-    # config_keyにマッチする環境のコンフィグクラスを読み込む
-    app.config.from_object(config[config_key])
