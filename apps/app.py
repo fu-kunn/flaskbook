16b53d9c77855fdca47ee5ecb7e8b1edcace8a35
fu-kunn <sky53908@gmail.com>
2023-01-30 17:03:14 +0900

    [Add]認証機能のエンドポイントを作成

--- apps/app.py
+++ apps/app.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+# from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -42,6 +42,10 @@ def create_app(config_key):
     from apps.crud import views as crud_views
     # register_blueprintを使いviewsのcrudをアプリへ登録する
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+    
+    # これから作成するauthパッケージからviewsをimportする
+    from apps.auth import views as auth_views
+    # register_blueprintを使いviewsのauthをアプリへ登録する
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
-
