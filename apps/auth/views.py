4c36127c5deb86d8f4e1046169cc83f819c7111a
fu-kunn <sky53908@gmail.com>
2023-02-01 09:21:40 +0900

    [Start]サインアップ完了時のリダイレクト先を変更

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -42,7 +42,7 @@ def signup():
     # GETパラメーターにnextキーが存在し、値がない場合はユーザーの一覧ページへリダイレクトする
     next_ = request.args.get("next")
     if next_ is None or not next_.startswith("/"):
-      next_ = url_for("crud.users")
+      next_ = url_for("detector.index")
     return redirect(next_)
 
   return render_template("auth/signup.html", form=form)
